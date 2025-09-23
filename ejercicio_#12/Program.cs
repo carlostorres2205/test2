50aa0211735bf0a00af46e619c7c55f4e13c8b62
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-09-22 23:56:42 -0600

    Entrega en proceso del ejercicio 12 cajero automatico

---
+++ ejercicio_#12/Program.cs
@@ -0,0 +1,35 @@
+﻿//Simulacion de cajero Automatico
+
+//Usar una variable global que represente el saldo de una cuenta bancaria 
+
+//Metodo locales para
+
+//Depositar dinero
+
+//retirar dinero ( validando que no exceda el saldo )
+
+//consultar saldo
+
+using System;
+
+class Program
+{
+    static int saldo = 1000; // variable global
+
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("-------Bienvenido a BAC--------");
+        Console.WriteLine("\nQue deseas hacer?\n1. Depositar saldo\n2. Retirar saldo\n3. Consultar saldo\n4.Salir");
+        int opcion = Convert.ToInt32(Console.ReadLine());
+
+        if (opcion == 1) ;
+        {
+            Console.WriteLine("Monto a depositar : ");
+            Depositar();
+        }
+
+
+    }
+
+}
