114ec91b825f07bde0d0bb138e267b3f97716e46
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-09-22 23:26:17 -0600

    Termine ejercicio #10 puntuacion

--- ejercicio_#10/Program.cs
+++ ejercicio_#10/Program.cs
@@ -1,6 +1,26 @@
 ﻿//Declara una variable global puntuacion. Cada vez que se llame al método GanarPuntos, la puntuación debe aumentar en 10 y mostrarse en pantalla.
 
-class program
+using System;
+
+class Program
 {
-    
+    //variable global
+    static int puntacion = 0;
+
+    static void Main(string[] args)
+    {
+        {
+            GanarPuntos();
+            GanarPuntos();
+            GanarPuntos();
+            GanarPuntos();
+            GanarPuntos();
+        } 
+
+    }
+    static void GanarPuntos() // metodo que aumenta la puntacion
+    {
+        puntacion += 10;
+        Console.WriteLine($"Tienes {puntacion} puntos ganados");
+    }
 }
--- puntuacion_#10.cs
+++
@@ -1,1 +0,0 @@
-//Declara una variable global puntuacion. Cada vez que se llame al método GanarPuntos, la puntuación debe aumentar en 10 y mostrarse en pantalla.
