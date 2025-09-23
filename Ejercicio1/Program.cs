1c78c6110259a2e8b7d95ae09b5f549a4ab5f6bb
asloaisiga <asloaisiga@uamv.edu.ni>
2025-09-22 20:42:51 -0600

    Ejercicios Alexa

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿//1.	Contador de accesos global
+//Declara una variable global que almacene el número de veces que se ha ejecutado un método. 
+//Cada vez que se llame al método, debe incrementar el contador y mostrarlo en pantalla.
+
+using System;
+namespace ContadorAccesosGlobal
+{
+    class Program
+    {
+        // Variable global para contar los accesos
+        static int contadorAccesos = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador de accesos global al método.");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Escriba la cantidad de veces que desea llamar al método:");
+            int cantidadLlamadas = Convert.ToInt32(Console.ReadLine());
+            if (cantidadLlamadas < 1)
+            {
+                Console.WriteLine("Por favor, ingrese un número válido mayor que 0.");
+            }
+            else
+            {
+                for (int i = 0; i < cantidadLlamadas; i++)
+                {
+                    MetodoConContador();
+                }
+            }
+            
+        }
+
+        static void MetodoConContador()
+        {
+            // Incrementar el contador cada vez que se llama al método
+            contadorAccesos++;
+            // Mostrar el número de accesos en pantalla
+            Console.WriteLine($"El método ha sido ejecutado {contadorAccesos} veces.");
+        }
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿//2.	Área de un rectángulo con variables locales
+//Crea un método que pida la base y altura de un rectángulo, use variables locales para calcular el área y la imprima en pantalla.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Calculadora de área de un rectángulo");
+
+        CalcularArea();
+
+    }
+    static void CalcularArea()
+    {
+        // Variables locales
+        double baseRectangulo;
+        double alturaRectangulo;
+        double area;
+
+        // Solicitar base y altura al usuario
+        Console.Write("Ingrese la base del rectángulo: ");
+        baseRectangulo = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese la altura del rectángulo: ");
+        alturaRectangulo = Convert.ToDouble(Console.ReadLine());
+
+        // Calcular el área
+        area = baseRectangulo * alturaRectangulo;
+
+        // Imprimir el área en pantalla
+        Console.WriteLine($"El área del rectángulo es: {area} unidades cuadradas");
+    }
+}
