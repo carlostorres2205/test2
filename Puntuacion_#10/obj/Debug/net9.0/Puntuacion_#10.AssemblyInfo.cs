56faa38f6b0813b76f2dcf781e6e07d5a17ef0c7
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-09-22 21:37:35 -0600

    agruegue ejercicio Puntuacion

--- Puntuacion_#10/obj/Debug/net9.0/Puntuacion_#10.AssemblyInfo.cs
+++ Puntuacion_#10/obj/Debug/net9.0/Puntuacion_#10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Puntuacion_#10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa67a3cb68a6d9ceca444f45b5da8a4754525efe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Puntuacion_#10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Puntuacion_#10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
